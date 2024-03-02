e84e522a79363993a107e5a6fe285002f25482a5
kimhyun <forhn37@gmail.com>
2024-03-02 14:10:14 +0900

    feat : sendFile 및 __dirname 확인

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 app.set('port', process.env.PORT || 3000);
 
-app.get('/',(req, res) => {
-  res.send('Hello, Express');
+app.get('/', (req, res) => {
+  // res.send('Hello, Express');
+  res.sendFile(path.join(__dirname, '/index.html'))
 });
-
+console.log(__dirname)
 app.listen(app.get('port'), ()=> {
   console.log(app.get('port'),'번 포트에서 대기 중');
 })
