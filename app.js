1c9fe5c14a54fa410180d13af98621a1569c49cf
kimhyun <forhn37@gmail.com>
2024-03-03 13:29:14 +0900

    feat : multer 설치

--- app.js
+++ app.js
@@ -9,10 +9,12 @@ dotenv.config()
 const app = express();
 app.set('port', process.env.PORT || 3000);
 
-app.use(morgan('dev'))
-app.use('/', express.static(path.join(__dirname, 'public')));
-app.use(expres.json());
-app.use(express.urlencoded({extended:false}));
+app.use(morgan('dev'))//요청과 응답에 대한 정보 기록
+// http메서드, 주소, http상태코드, 응답속도, - 응답바이트
+app.use('/', express.static(path.join(__dirname, 'public'))); //정적파일 라우팅 설정
+app.use(express.json()); // 요쳥한 데이터가 json형식일때 자바스크립트 형식으로 바꿈
+app.use(express.urlencoded({extended:false})); // get 요청시 쿼리스트링의 데이터를 자바스크립트 형식으로 자동으로 파싱하여 body에 담음
+// false이면 querystring (내부모듈사용), true이면 qs(라이브러리 사용)
 app.use(cookieParser(process.env.COOKIE_SECRET));
 app.use(session({
   reasve:false,
@@ -24,12 +26,7 @@ app.use(session({
   },
   name:'session-cookie',
 }));
-
-
-
-
-
-
+console.log(process.env)
 
 
 
@@ -43,8 +40,9 @@ app.get('/', (req, res, next) => {
   console.log('GET / 요청에서만 실행됩니다.');
   next();
 }, (req, res) => {
-throw new Error('에러는 에러처리 미들웨어로 갑니다. ')
+  throw new Error('에러는 에러처리 미들웨어로 갑니다. ')
 });
+
 
 
 app.use((err, req, res, next)=> {
